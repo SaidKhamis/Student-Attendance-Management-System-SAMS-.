11d4e252ff6723c754b3a4fc48717b6f0e98ef21
SaidKhamis <saidkhamis2306@gmail.com>
2025-03-20 08:53:53 +0300

    Base class methods
    Methods:
     - To add the student in the class database
     - To display available students within the specific class

---
+++ base class methods.cpp
@@ -0,0 +1,10 @@
+	// Function to add a student
+	void add_student() {
+		cout << "Student added: ID = " << student_id << ", Name = " << student_name << endl;
+	}
+	
+		// Function to display student details
+	void display_student() const {
+		cout << "ID: " << student_id << " | Name: " << student_name << endl;
+	}
+};
