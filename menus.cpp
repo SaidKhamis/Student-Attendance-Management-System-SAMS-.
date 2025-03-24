d89dfd9cc82af4903e3aca75cf2e40d4f5072d53
SaidKhamis <saidkhamis2306@gmail.com>
2025-03-24 11:12:34 +0300

    menu options
    The choices displayed firstly to the user that allocate user to accomplish his want

---
+++ menus.cpp
@@ -0,0 +1,7 @@
+		//Display menu options.
+		cout << "\n   ***  STUDENT ATTENDANCE MANAGEMENT SYSTEM  ***   \n";
+		cout << "1. Add Student.\n";
+		cout << "2. Mark Attendance.\n";
+		cout << "3. Display Students.\n";
+		cout << "4. Display Attendance.\n";
+		cout << "5. Exit.\n";
