73cfe65e4b21e05c69c5d418fe5b2eb588b57690
SaidKhamis <saidkhamis2306@gmail.com>
2025-03-25 08:25:29 +0300

    for the user choice 2, Marking attendance
    - User can take attendance and here will be needed to enter the date of marking attendance from students

---
+++ case 2-current date.cpp
@@ -0,0 +1,6 @@
+		case 2: 
+			{	
+				if (currentDate == "") {  // If the date hasn't been set yet
+					cout << "\n Enter Date (YYYY-MM-DD): ";
+					cin >> currentDate;  // Set the date for the current attendance session
+				}
