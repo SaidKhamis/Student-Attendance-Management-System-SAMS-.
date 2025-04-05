034bc2f84977011fe33a7b472dc2b2046ff79427
SaidKhamis <saidkhamis2306@gmail.com>
2025-04-05 09:23:21 +0300

    Update case 1.cpp

--- case 1.cpp
+++ case 1.cpp
@@ -1,16 +1,23 @@
 case 1: 
-	{
-		//Add new student
-		int id;
-		string name;
-		cout << "\n Enter Student ID: ";
-		cin >> id;
-		cin.ignore();
-		cout << " Enter Student Name: ";
-		getline(cin, name);
+{
+	int exit;
+        do {
+	    //Add new student
+	    int id;
+	    string name;
+	    cout << "\n Enter Student ID: ";
+	    cin >> id;
+	    cin.ignore();
+	    cout << " Enter Student Name: ";
+  	    getline(cin, name);
 		
-		Student* newStudent = new Student(id, name);
-		students.push_back(newStudent);
-		cout << "  New student added successfully!\n";
-		break;
-	}		
+	    Student* newStudent = new Student(id, name);
+	    students.push_back(newStudent);
+		
+	    cout << "Press 0 to Continue / 1 to exit : ";
+	    cin >> exit;	
+	}
+	while(exit==0);
+	cout << "  New student added successfully!\n"; 
+	break;
+}		
