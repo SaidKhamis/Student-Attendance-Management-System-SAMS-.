4cc3fd3977c3459983c8112f581303d10db333b1
SaidKhamis <saidkhamis2306@gmail.com>
2025-04-10 18:06:21 +0300

    Update case 2-current date.cpp

--- case 2-current date.cpp
+++ case 2-current date.cpp
@@ -1,6 +1,6 @@
 		case 2: 
 			{	
 				if (currentDate == "") {  // If the date hasn't been set yet
-					cout << "\n Enter Date (YYYY-MM-DD): ";
+					cout << "\n Enter Date (ie. DD-MM-YYYY): ";
 					cin >> currentDate;  // Set the date for the current attendance session
 				}
