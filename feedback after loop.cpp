bd0515fefd6b0694b497890560e71f92c2fb53c0
SaidKhamis <saidkhamis2306@gmail.com>
2025-04-17 12:53:06 +0300

    feedback in case 1.cpp

--- feedback after loop.cpp
+++ feedback after loop.cpp
@@ -1,22 +1,3 @@
-			case 1: 
-			{	
-				int exit;
-			 	do{
-			 		//Add new student
-					int id;
-					string name;
-					cout << "\n Enter Student ID: ";
-					cin >> id;
-					cin.ignore();
-					cout << " Enter Student Name: ";
-					getline(cin, name);
-					
-					Student* newStudent = new Student(id, name);
-					students.push_back(newStudent);
-					
-					cout << "\nPress 0-continue or 1-close : ";
-					cin >> exit;
-				}	while(exit==0);		
-				cout << "  Student added successfully!\n";
-				break;
-			}
+students.push_back(newStudent)
+cout << "  Students added successfully!\n";
+break;
