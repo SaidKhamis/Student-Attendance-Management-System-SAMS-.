bfffc2229b737e77a22a462911f51572acff2edc
SaidKhamis <saidkhamis2306@gmail.com>
2025-04-18 12:41:47 +0300

    Update Case 5.cpp

--- Case 5.cpp
+++ Case 5.cpp
@@ -1,7 +1,18 @@
-case 5 {
+case 5: {
   //delete student
-  students* student(id, name);
+  cout << " - STUDENT'S LIST - ";
+  for (const auto& student : students) {
+    student->display_student();
+  }
+  int id;
+  cout << "Enter student ID to delete: ";
+  cin >> id;
 
-  
-  
+  for (auto it=students.begin(); it!=students.end(); it++) {
+    if((*it)->student_id==id){
+      students.erase(it);
+      cout << "Student deleted successfully!\n";
+    }
+  }
+  break; 
 }
