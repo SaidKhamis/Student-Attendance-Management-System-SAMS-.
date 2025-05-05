83655d2d1fdbd4f74b8259e829d6529e6569585d
SaidKhamis <saidkhamis2306@gmail.com>
2025-05-05 15:57:39 +0300

    Update 1st version.cpp

--- 1st version.cpp
+++ 1st version.cpp
@@ -74,25 +74,8 @@ int main() {
 		switch (choice){
 			case 1: 
 			{	
-				int exit;
-				do{
-					//Add new student
-					int id;
-					string name;
-					cout << "\n Enter Student ID: ";
-					cin >> id;
-					cin.ignore();
-					cout << " Enter Student Name: ";
-					getline(cin, name);
-				
-					Student* newStudent = new Student(id, name);
-					students.push_back(newStudent);
-					
-					cout << "\nPress 0-continue or 1-close : ";
-					cin >> exit;
-				}	while(exit==0);		
-				cout << students.add_student();	
-				break;
+			    add_student();
+			    break;
 			}
 			
 			case 2: 
